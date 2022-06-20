0ce046363a39006785d292563430d48d96175092
BBO1111 <babenko.bohdan@lll.kpi.ua>
2022-06-20 14:50:13 +0300

    Initial Commit

---
+++ Tetris/TetrisApp.swift
@@ -0,0 +1,17 @@
+//
+//  TetrisApp.swift
+//  Tetris
+//
+//  Created by mac on 20.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct TetrisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
